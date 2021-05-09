7e04dba5fbaeba0d6e7a2bbf864e860efa06f71c
Ellis Kesterton <ellis@diagnosticbox.com>
2021-05-09 14:59:42 +0100

    Keyboard scancode detection!

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -8,6 +8,8 @@ use pic8259_simple::ChainedPics;
 pub static PICS: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe { ChainedPics::new(32, 40) });
 
+// TODO: cpuio::Port vs x86_64::instructions::port::Port
+
 
 const PIC1_CMD: Port<u8> = unsafe { Port::new(0x20) };
 const PIC1_DATA: Port<u8> = unsafe { Port::new(0x21) };
@@ -21,6 +23,7 @@ lazy_static! {
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
         idt[32].set_handler_fn(timer_interrupt_handler);
+        idt[33].set_handler_fn(keyboard_handler);
         idt
     };
 }
@@ -40,34 +43,30 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame,
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    log!(".");
+    //log!(".");
+
+    PIC1_CMD.write(0x20);
+}
+
+extern "x86-interrupt" fn keyboard_handler(_stack_frame: InterruptStackFrame) {
+    log!("key pressed");
+
+    let mut port: Port<u8> = unsafe { Port::new(0x60) };
+    let scancode = port.read();
+
+    log!("scancode: {}\n", scancode);
 
     PIC1_CMD.write(0x20);
 }
 
 
-fn init_pic() {
 
+fn init_pic() {
     unsafe { PICS.lock().initialize() };
 
-    let new_mask = 0b11111110;
+    let new_mask = 0b11111100;
     PIC1_DATA.write(new_mask);
 
     let mask = PIC1_DATA.read();
     log!("PIC1 mask: {}\n", mask);
-
-    /*let mut wait_port: Port<u8> = unsafe { Port::new(0x80) };
-    let mut wait = || { wait_port.write(0) };
-
-    PIC1_CMD.write(0x11); // 0x10?
-    wait();
-
-    PIC1_DATA.write(32);
-    wait();
-
-    PIC1_DATA.write(4);
-    wait();
-
-    PIC1_DATA.write(0x01);
-    wait();*/
 }
